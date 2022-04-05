e561ff12252bc582bbeb48156804300dbbe3640a
mohamed-elzohery <m.zohery1998@gmail.com>
2022-04-06 00:48:03 +0200

    Modify logo navogate to

--- layout/header.php
+++ layout/header.php
@@ -14,7 +14,7 @@
   
 <nav class=" bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
   <div class="container flex flex-wrap justify-between items-center mx-auto">
-    <a href="https://flowbite.com" class="flex items-center">
+    <a href="./home.php" class="flex items-center">
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/ZDF_logo%21_Logo_2021.svg/1200px-ZDF_logo%21_Logo_2021.svg.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Elzohery</span>
     </a>
